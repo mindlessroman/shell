491ff03091a3a35a5dde3fe2bce345828279c923
Hannah K @mindlessroman <mindless.roman@gmail.com>
2016-04-16 09:07:12 -0400

    Initial Commit - barebones

---
+++ isbn_validator.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#take in number, strip dashes, refuse more than 10 digits
+if [${1} -ne 10]
+  echo "i'm a fart monster
+fi
+#do a test
+#return true if valid isbn
+#return false if invalid isbn
