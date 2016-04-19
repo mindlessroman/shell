1f21c6345dc7914f039810715589b8bdf95efb47
Hannah K @mindlessroman <mindless.roman@gmail.com>
2016-04-18 22:36:37 -0400

    Fleshing out more - figuring out for loops in shel.

--- isbn_validator.sh
+++ isbn_validator.sh
@@ -18,8 +18,24 @@ elif [[ ${#intake} -ne 10 ]]; then
    echo "You entered a number string that has a length not equal to 10"
 fi
 
-#zsh - 1-indexes
-#exploe the string into an array
+#if [[ $intake =~ ]] #10digit number
+
+#zsh - 0-indexes
+#explode the string into an array
 #check for letters? if we're assuming clean input, then whatever
 
-echo "Is the provided number a valid ISBN?:"
+#$index = 10
+$is_valid = false
+$total = 0
+
+#not how for loops work apparently in shell
+for (( i = 10; i >= 0; i++)); do
+    $total = $total + (i * ${intake:i})  
+done
+
+if [[ $total % 11 -eq  0 ]]
+    $is_valid = true
+fi
+
+echo -n "Is the provided number a valid ISBN?:"
+echo $is_valid
